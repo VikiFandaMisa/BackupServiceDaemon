43601dc7630b27bb231b17677da7c728149cbdb3
Michal Černý <cernyymichal@gmail.com>
2020-04-20 19:10:13 +0200

    initial comit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackupServiceDaemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
