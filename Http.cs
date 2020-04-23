1bb6388c6ce0ff51ac77b3437fa3146dd28316f0
Michal Černý <cernyymichal@gmail.com>
2020-04-23 22:11:18 +0200

    changes :)

--- Http.cs
+++ Http.cs
@@ -9,19 +9,20 @@ namespace BackupServiceDaemon
 {
     public static class Http
     {
-        public static string URL { get; set; } = "http://localhost:5001";
-        public static Computer[] GetComputer()
+        //VSUDE SEND AUTHORIZATION: Bearer *token*
+        public static string URL { get; set; } = "http://localhost:5001/api";
+        public static Computer GetComputer()
         {
             using (var computer = new HttpClient())
             {
                 computer.BaseAddress = new Uri(URL);
 
-                var responseTask = computer.GetAsync("computer");
+                var responseTask = computer.GetAsync("computers/self");
                 responseTask.Wait();
                
                 if (responseTask.Result.IsSuccessStatusCode)
                 {
-                    var readTask = responseTask.Result.Content.ReadAsAsync<Computer[]>();
+                    var readTask = responseTask.Result.Content.ReadAsAsync<Computer>();
                     readTask.Wait();
 
                     return readTask.Result;
@@ -35,16 +36,15 @@ namespace BackupServiceDaemon
             using (var computer = new HttpClient())
             {
                 computer.BaseAddress = new Uri(URL);
-                
 
                 var computerRegistration = new ComputerRegistration() { Hostname = PCInfo.GetHostName(), MAC = PCInfo.GetMAC(), IP = PCInfo.GetIP() };
 
-                var Task = computer.PostAsJsonAsync<ComputerRegistration>("client", computerRegistration);
+                var Task = computer.PostAsJsonAsync<ComputerRegistration>("computers/registration", computerRegistration);
                 Task.Wait();
 
                 if (Task.Result.IsSuccessStatusCode)
                 {
-
+                    //CATCH COMPUTER ID
                     var readTask = Task.Result.Content.ReadAsAsync<ComputerRegistration>();
                     readTask.Wait();
 
--- Models/ComputerRegistration.cs
+++ Models/ComputerRegistration.cs
@@ -5,7 +5,6 @@ namespace BackupServiceDaemon.Models
     public class ComputerRegistration
     {
         public string Hostname { get; set; }
-        public string Password { get; set; }
         public string MAC { get; set; }
         public string IP { get; set; }
     }
