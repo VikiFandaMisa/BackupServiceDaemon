602f171b9872983f76c9c83e0f79e241372ae626
krizfrantisek@sssvt.cz <krizfrantisek@sssvt.cz>
2020-05-01 21:36:11 +0200

    full backup added

--- BackupAlgorithms/FullBackup.cs
+++ BackupAlgorithms/FullBackup.cs
@@ -1,12 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace BackupServiceDaemon.BackupAlgorithms
 {
     public class FullBackup : IBackup
     {
+        public string Source { get; set; }
+        public string Target { get; set; }
         public void Run(IProgress<BackupProgress> progress) {
             System.Console.WriteLine("Full backup");
             progress.Report(new BackupProgress() { Percentage = 100 });
         }
+        public void Backup() {
+            Utils.CopyDirectory(Source, Target);
+        }
+        
     }
+    
 }
