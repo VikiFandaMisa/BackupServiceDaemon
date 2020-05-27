697cfd52b7a6bfc3b24b0efb3c8c3c8158d27bcc
Michal Černý <cernyymichal@gmail.com>
2020-05-27 19:27:18 +0200

    add future fs templates

---
+++ Backuping/FileSystemAPIs/FTPFileSystemAPI.cs
@@ -0,0 +1,25 @@
+using System.IO;
+
+namespace BackupServiceDaemon.Backuping.FileSystemAPIs
+{
+    public class FTPFileSystemAPI : IFileSystemAPI {
+        public string[] GetFiles(string directory) {
+            throw new System.NotImplementedException();
+        }
+        public string[] GetDirectories(string directory) {
+            throw new System.NotImplementedException();
+        }
+        public void CreateDirectory(string directory) {
+            throw new System.NotImplementedException();
+        }
+        public void CopyFile(string source, string target) {
+            throw new System.NotImplementedException();
+        }
+        public string CombinePath(params string[] path) {
+            throw new System.NotImplementedException();
+        }
+        public string GetFileName(string path) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
